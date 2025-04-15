dc4bca24aa0e759d696c37c24225734923057cfa
piinabisket <piinabun@gmail.com>
2025-04-15 14:35:20 -0700

    Changed TFT Library for use with Mega 2560 rather than Uno. Buttons now only update when pressed

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,8 @@
 //           http://www.educ8s.tv           //
 /////////////////////////////////////////////
 
-#include <Adafruit_TFTLCD.h> 
+//#include <Adafruit_TFTLCD.h> 
+#include <MCUFRIEND_kbv.h>
 #include <Adafruit_GFX.h>    
 #include <TouchScreen.h>
 
@@ -14,10 +15,10 @@
 #define LCD_RD A0 
 #define LCD_RESET A4 
 
-#define TS_MINX 100
-#define TS_MINY 100
-#define TS_MAXX 960
-#define TS_MAXY 930
+#define TS_MINX 105
+#define TS_MINY 85
+#define TS_MAXX 970
+#define TS_MAXY 940
 
 #define YP A2  // must be an analog pin, use "An" notation!
 #define XM A3  // must be an analog pin, use "An" notation!
@@ -33,8 +34,9 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-//GFXcanvas1 tft(320, 240);
+MCUFRIEND_kbv tft;
+//Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+GFXcanvas1 canvas(38, 38);
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 boolean buttonEnabled = true;
@@ -43,21 +45,26 @@ unsigned long time_now = 0;
 TSPoint p;
 int humiditySetpoint = 72;
 int tempSetpoint = 72;
-bool tempButton = true;
+bool tempButtonState = true;    //flag to indicate if button has been pressed
 
 void setup() {
   Serial.begin(9600);
   Serial.print("Starting...");
-  
   tft.reset();
-  tft.begin(0x9341);
-  tft.setRotation(1);
+  tft.begin(tft.readID());
+  tft.setRotation(3);
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("reading id....0x");
+  delay(500);
+  Serial.println(tft.readID(), HEX);
   tft.fillScreen(BLACK);
 
 }
 
 void loop() 
 {
+  
   time_now = millis();  //get current time
   // Delay between measurements.
   
@@ -71,30 +78,31 @@ void loop()
       p.y = map(p.y, TS_MINY, TS_MAXY, 0, 240);
 
       if(p.x>225 && p.x<260 && p.y>66 && p.y<96){
-        if(tempButton){
+        if(!tempButtonState){
           tempSetpoint--;
         }
-        tempButton = false;
+        tempButtonState = true;
         pinMode(XM, OUTPUT);
         pinMode(YP, OUTPUT);
-        tft.fillRect(223, 64, 38, 32, BLACK);
+        tft.fillRect(223, 64, 38, 32, BLACK);   //Temp Button Down ON State
         tft.fillRect(225, 70, 35, 32, WHITE);
         tft.fillRect(226, 71, 33, 24, BLACK);
+        tft.drawTriangle(225+8,68+12, (225+35)-10,68+12, 225+17,68+21, WHITE);
       }
       else if(p.x>275 && p.x<310 && p.y>66 && p.y<96){
-        if(tempButton){
+        if(!tempButtonState){
           tempSetpoint++;
         }
-        tempButton = false;
+        tempButtonState = true;
         pinMode(XM, OUTPUT);
         pinMode(YP, OUTPUT);
-        tft.fillRect(273, 64, 38, 32, BLACK);
+        tft.fillRect(273, 64, 38, 32, BLACK);   //Temp Button Up ON State
         tft.fillRect(275, 70, 35, 32, WHITE);
         tft.fillRect(276, 71, 33, 24, BLACK);
+        tft.drawTriangle(275+8,66+21, (275+35)-9,66+21, 275+17,66+8, WHITE);
       } 
     }
   }
-  tempButton = true;
   pinMode(XM, OUTPUT);
   pinMode(YP, OUTPUT);
   //Draw Header
@@ -118,11 +126,15 @@ void loop()
   tft.print(humiditySetpoint);
   tft.println("%");
   tft.drawRect(0,0,320,240,WHITE);
+  if(tempButtonState){
+    tft.fillRect(223, 64, 38, 38, BLACK);   //Temp Down Button OFF State
+    tft.fillRect(225, 66, 35, 30, WHITE);
+    tft.fillTriangle(225+8,66+10, (225+35)-10,66+10, 225+17,66+19, BLACK);
+    tft.fillRect(273, 64, 38, 38, BLACK);
+    tft.fillRect(275, 66, 35, 30, WHITE);   //Temp Up Button OFF State
+    tft.fillTriangle(275+8,66+19, (275+35)-9,66+19, 275+17,66+10, BLACK);
+  }
+  tempButtonState = false;
 
-  tft.fillRect(223, 64, 38, 38, BLACK);
-  tft.fillRect(273, 64, 38, 38, BLACK);
-  tft.fillRect(225, 66, 35, 30, WHITE);
-  tft.fillRect(275, 66, 35, 30, WHITE);
-  //display.drawBitmap(0, 0, tft.getBuffer(), tft.width(), tft.height(), 0xFFFF, 0x0000);
 }
 
